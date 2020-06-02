6180ab9904d29931544876135351a2facba627ee
han <han.mickey@gmail.com>
2020-06-02 13:28:09 +0900

    Create week3_hw.py

---
+++ Sparta/etc/week3_hw.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+music = soup.select('#old_content > table > tbody > tr')
+soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for item in music:
+
+    artist = item.select_one('td.info > a.artist.ellipsis')
+    title = item.select_one('td.info > a.title.ellipsis')
+
+    if artist is not None:
+        artist = a_tag.text                                      
+        songName = item.select_one('td.info > a.title.ellipsis').text               
+        print(artist,songName)
